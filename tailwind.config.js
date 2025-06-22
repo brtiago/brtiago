f21218de59deebebfa2baae032c750caf2434007
brtiago <tgribeiro@gmail.com>
2025-06-22 09:57:31 -0300

    feat: adding tailwindconfig

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          amber: {
+            300: '#fcd34d',
+            500: '#f59e0b',
+            600: '#d97706',
+          },
+        },
+        spacing: {
+          '128': '32rem',
+        },
+      },
+    },
+  }
